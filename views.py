53d01afa51410375967c9f1474edb3ec0a033cae
Aljannah <hikmahaj55@gmail.com>
2017-04-05 23:34:27 +0700

    isi file app

--- views.py
+++ views.py
@@ -1,17 +1,17 @@
 from flask import render_template, flash, redirect
 from app import app
-from forms import LoginForm
+#from .forms import LoginForm
 
+#def index():
+ 
 #@app.route('/login', methods=['GET', 'POST'])
 @app.route('/index')
 def login():
-    form = LoginForm()
-    
-    return render_template('login.html',
-			    title='Sign In',
-			    form=form)
-
+    #form = LoginForm()
+    return render_template('form.html',
+                           title='Submit',
+                           form=form)
 @app.route('/kuis')
 def kuis():
-   
-    return render_template('kuis.py')
+
+	return render_template('kuis.py')
